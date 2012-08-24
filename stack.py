2fa3d91042cb66cd9101dff0e5ac0b284455a9d7
Pete Richardson <pete@peterichardson.com>
2012-08-23 21:17:30 -0700

    Misc cleanup of stack.py
    added docstrings
    made peek clone the top item
    make pop use peek

--- stack.py
+++ stack.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
+'''stack.py - a simple stack implementation'''
+
 import unittest
 from pprint import pformat
 import logging
+import copy
+
 
 class Stack:
     def __init__(self):
@@ -12,14 +16,18 @@ class Stack:
         self.stack.insert(0, item)
 
     def pop(self):
-        answer = self.stack[0]
+        '''Remove the top item from the top of the stack 
+           and return it'''
+        answer = self.peek()
         self.stack = self.stack[1:]
         return answer
 
     def peek(self):
-        return(self.stack[0])
+        '''Return a copy of the top item on the stack'''
+        return(copy.copy(self.stack[0]))
 
     def count(self):
+        '''Return the number of items on the stack'''
         return(len(self.stack))
 
     def dump(self):
@@ -29,6 +37,7 @@ class Stack:
 class TestStack(unittest.TestCase):
     def test_push(self):
         s = Stack()
+        self.assertEqual(s.count(), 0)
         s.push("pete")
         p = s.peek()
         self.assertEqual(s.count(), 1)
@@ -43,6 +52,16 @@ class TestStack(unittest.TestCase):
         answer = s.pop()
         self.assertEqual(answer, "Peter")
         self.assertEqual(s.count(), 1)
+
+    def test_count(self):
+        s = Stack()
+        self.assertEqual(0, s.count())
+        s.push("One")
+        self.assertEqual(1, s.count())
+        s.push("Two")
+        self.assertEqual(2, s.count())
+        kk
+
 
 if __name__ == "__main__":
     #logging.basicConfig(level=logging.DEBUG)
