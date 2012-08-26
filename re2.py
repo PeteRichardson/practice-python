6dea83d568b1557350a31be0f56937ac054526ae
Pete Richardson <pete@peterichardson.com>
2012-08-26 08:57:59 -0700

    Count the n most popular characters in a text sample
    Use /usr/share/dict/words and n=5

--- re2.py
+++ re2.py
@@ -2,9 +2,13 @@
 
 from collections import Counter
 
+n = 5
+input_file = "/usr/share/dict/words"
 
 words = Counter()
-for l in open("changes.py", "r"):
-    words.update(l.split())
-for word, count in words.most_common(10):
-    print "%4d: %s" % (count, word)
+for l in open(input_file, "r"):
+    words.update(l.strip())
+
+print "Top %d Popular Chars in %s" % (n, input_file)
+for word, count in words.most_common(n):
+    print "%7d: %s" % (count, word)
