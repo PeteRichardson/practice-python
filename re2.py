6aa4c9468007ce68c89fa09285f4f23a71e770f2
Pete Richardson <pete@peterichardson.com>
2012-08-26 09:26:06 -0700

    Added a word counter function
    Still need to refactor so we can reuse the common Counter code.
    (i.e. refactor to where the only different code is how to split the line)

--- re2.py
+++ re2.py
@@ -1,18 +1,45 @@
 #!/usr/bin/env python
 
 from collections import Counter
-from optparse import OptionParser
 
-parser = OptionParser()
-parser.add_option('-n', dest="n", default=5, action="store")
-parser.add_option('-f', '--file', dest="input_file",
-    default="/usr/share/dict/words", action="store")
-options, args = parser.parse_args()
 
-words = Counter()
-for l in open(options.input_file, "r"):
-    words.update(l.strip())
+def count_chars(input_file):
+    '''return a collections.Counter with char counts for the
+       specified file'''
+    chars = Counter()
+    for l in open(options.input_file, "r"):
+        chars.update(l.strip())
+    return chars, "Chars"
 
-print "Top %d Popular Chars in %s" % (options.n, options.input_file)
-for word, count in words.most_common(options.n):
-    print "%7d: %s" % (count, word)
+def count_words(input_file):
+    '''return a collections.Counter with word counts for the
+       specified file'''
+    words = Counter()
+    for l in open(options.input_file, "r"):
+        words.update(l.strip().split())
+    return words, "Words"
+
+
+def get_options():
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    parser.add_option('-n', dest="n", default=5, action="store")
+    parser.add_option('-f', '--file', dest="input_file",
+        default="/usr/share/dict/words", action="store")
+    return parser.parse_args()
+
+
+def print_report(char_counter, options, item_type):
+    '''dump the most popular items in a counter'''
+    print "Top %d Popular %s in %s" % (options.n, item_type, options.input_file)
+    for char, count in char_counter.most_common(options.n):
+        print "%7d: %s" % (count, char)
+
+if __name__ == '__main__':
+    options, args = get_options()
+    items, item_type = count_chars(options.input_file)
+    print_report(items, options, item_type)
+    print "-----------------"
+    items, item_type = count_words(options.input_file)
+    print_report(items, options, item_type)
