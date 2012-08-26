446367e25d7265f5fb50e8542a246bbc43ee16f2
Pete Richardson <pete@peterichardson.com>
2012-08-26 16:38:39 -0700

    Added unittests for minmax

--- minmax.py
+++ minmax.py
@@ -6,6 +6,7 @@
 
 from pprint import pprint
 import logging
+import unittest
 
 
 def is_min(old_direction, new_direction):
@@ -43,21 +44,42 @@ def get_minmax(ints):
         old_direction = new_direction
         current = next
 
-
     return results
 
 
-def read_file(fname):
-    return [int(l.strip()) for l in open(fname, "r") if l[0] != "#"]
+class Test_minmax(unittest.TestCase):
 
+    def test_simplemax(self):
+        ints = [4,5,4]
+        results = get_minmax(ints)
+        self.assertEqual(results, [(1, 5, "MAX")])
 
+    def test_simplemin(self):
+        ints = [4,3,4]
+        results = get_minmax(ints)
+        self.assertEqual(results, [(1, 3, "MIN")])
+
+    def test_allsame(self):
+        ints = [4,4,4]
+        results = get_minmax(ints)
+        self.assertEqual(results, [])
+
+    def test_allincreasing(self):
+        ints = [3,4,5]
+        results = get_minmax(ints)
+        self.assertEqual(results, [])
+
+    def test_alldecreasing(self):
+        ints = [5,4,3]
+        results = get_minmax(ints)
+        self.assertEqual(results, [])
+
+    def test_complex(self):
+        ints = [5,7,9,8,5,4,2,1,5,6]
+        results = get_minmax(ints)
+        self.assertEqual(results, [(2,9,"MAX"), (7,1,"MIN")])
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.ERROR)
-    fname = "data.txt"
-    ints = read_file(fname)
-    results = get_minmax(ints)
-
-    for loc, val, type in results:
-        print "%3d %4d %s" % (loc, val, type)
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
