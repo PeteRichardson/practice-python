cf8e18461bafd42ff619255f27c87333884405c0
Pete Richardson <pete@peterichardson.com>
2012-08-26 09:08:00 -0700

    Add option parsing using opt parse

--- re2.py
+++ re2.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
 from collections import Counter
+from optparse import OptionParser
 
-n = 5
-input_file = "/usr/share/dict/words"
+parser = OptionParser()
+parser.add_option('-n', dest="n", default=5, action="store")
+parser.add_option('-f', '--file', dest="input_file",
+    default="/usr/share/dict/words", action="store")
+options, args = parser.parse_args()
 
 words = Counter()
-for l in open(input_file, "r"):
+for l in open(options.input_file, "r"):
     words.update(l.strip())
 
-print "Top %d Popular Chars in %s" % (n, input_file)
-for word, count in words.most_common(n):
+print "Top %d Popular Chars in %s" % (options.n, options.input_file)
+for word, count in words.most_common(options.n):
     print "%7d: %s" % (count, word)
