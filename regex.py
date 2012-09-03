ad98267b1404ee2c9694152dd8901465d8ec04f7
Pete Richardson <pete@peterichardson.com>
2012-09-03 12:07:02 -0700

    changes.py -> changes.txt to avoid nose discovery errors

--- regex.py
+++ regex.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
+''' script to search for regex matching lines in a file '''
 
 import re
 from pprint import pprint
 
 
-fname = "changes.py"
+FILENAME = "changes.txt"
 
-pprint([l.strip() for l in open(fname, "r") if re.search("project", l)])
+pprint([l.strip() for l in open(FILENAME, "r") if re.search("project", l)])
 
