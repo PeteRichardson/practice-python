e2dd6b20e78abc82aba1664f98000fe1e2ebcd6e
Pete Richardson <pete@peterichardson.com>
2012-09-03 11:43:18 -0700

    Added another bsort implementation.   Presumably the same as the other bsort

---
+++ newbsort/bsort.py
@@ -0,0 +1,32 @@
+''' bsort.py - a module to sort arrays using bubblesort '''
+#!/usr/bin/env python
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+def bubblesort(array):
+    ''' repeatedly walk the array swapping items that are
+        out of order until no swaps are possible '''
+
+    if array == None:
+        return array
+
+    array_copy = list(array)
+    if (len(array_copy) <= 1):
+        return array_copy 
+
+    item_swapped = True
+    while (item_swapped == True):
+        item_swapped = False
+        for n in range(0, len(array_copy) - 1):
+            if array_copy[n] > array_copy[n+1]:
+                item_swapped = True
+                logger.debug("swapping %d [%s] with %d [%s]" %
+                    (n, str(array_copy[n]), n+1, str(array_copy[n+1])))
+                array_copy[n], array_copy[n+1] = array_copy[n+1], array_copy[n]
+    return array_copy
+
+if __name__ == "__main__":
+    import unittest
+    unittest.main("test_bsort")
---
+++ newbsort/test_bsort.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+'''unit tests for bubblesort'''
+
+import unittest
+from bsort import bubblesort
+
+
+class Test_Bubblesort(unittest.TestCase):
+
+    def do_one(self, array):
+        self.assertEqual(bubblesort(array), sorted(array))
+
+
+    def test_simple(self):
+        ''' test_simple '''
+        self.do_one([1, 5, 2])
+
+
+    def test_none(self):
+        ''' test_None '''
+        self.assertEqual(None, bubblesort(None))
+
+
+    def test_one(self):
+        ''' test_one '''
+        self.do_one([1])
+
+
+    def test_odd(self):
+        ''' test_odd '''
+        self.do_one([1, 5, 10])
+
+
+    def test_even(self):
+        ''' test_even '''
+        self.do_one([5, 2, 7, 8])
+
+
+    def test_tuple(self):
+        ''' test_tuple '''
+        self.do_one((3, 2, 5))
+
+ 
+    def test_nondestructive(self):
+        ''' test_nondestructive '''
+        array = [7, 9, 3, 2]
+        after = bubblesort(array)
+        self.assertEqual(array[0], 7)
+        self.assertEqual(after, sorted(array))
+
+if __name__ == "__main__":
+    unittest.main()
+
