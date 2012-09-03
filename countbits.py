6a99d1030b0e9ec74113bd2ddfa7075ea4c8e7a9
Pete Richardson <pete@peterichardson.com>
2012-09-03 12:14:07 -0700

    fix pylint errors in countbits

--- countbits.py
+++ countbits.py
@@ -6,32 +6,40 @@
 import unittest
 
 
-def countbits(hex):
-    sum = 0
-    remainder = hex
+def countbits(hex_val):
+    ''' countbits '''
+    bit_count = 0
+    remainder = hex_val
     while remainder > 0:
-        sum = sum + (remainder & 0x1)
+        bit_count = bit_count + (remainder & 0x1)
         remainder = remainder >> 1
-    return sum
+    return bit_count
 
 lookuptable = [0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4]
 
 
-def countbits2(hex):
-    sum = 0
-    remainder = hex
+def countbits2(hex_val):
+    ''' countbits2 '''
+    bit_count = 0
+    remainder = hex_val
     while remainder > 0:
         byte = remainder & 0xF
-        sum = sum + lookuptable[byte]
+        bit_count = bit_count + lookuptable[byte]
         remainder = remainder >> 4
-    return sum
+    return bit_count
 
 
 class TestCountBits(unittest.TestCase):
+    ''' testcases '''
 
     def test_ints(self):
-        for i in range(0, 2 ** 20):
+        ''' test_ints '''
+        for i in range(0, 2 ** 10):
             self.assertEqual(countbits(i), countbits(i))
+
+    def test_2(self):
+        ''' test 2 '''
+        self.assertEqual(countbits(2), 1)
 
 if __name__ == "__main__":
     unittest.main()
