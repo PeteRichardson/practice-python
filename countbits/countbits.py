030a8a99aa469aa5dda5864c608fd0027a7d3584
Pete Richardson <pete@peterichardson.com>
2012-09-03 18:23:50 -0700

    Adding another copy of count bits, copied from the whiteboard

---
+++ countbits/countbits.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+''' countbits.py - a module that counts bits two ways '''
+
+def countbits(value):
+    ''' count bits by shifting one at a time '''
+    if value == None:
+        raise RuntimeError("Expected Integer, Got None")
+    bitcount = 0
+    while value > 0:
+        bitcount = bitcount + (value & 0x1)
+        value = value >> 1
+    return bitcount
+
+lookup_table = [0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4]
+def countbits2(value):
+    ''' count bits by shifting 4 at a time '''
+    if value == None:
+        raise RuntimeError("Expected Integer, Got None")
+    bitcount = 0
+    while value > 0:
+        bitcount = bitcount + lookup_table[value & 0xF]
+        value = value >> 4
+    return bitcount
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main("test_countbits")
---
+++ countbits/test_countbits.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+''' test_countbits.py - test cases for countbits functions '''
+
+import unittest
+from countbits import countbits, countbits2
+
+class TestCountbits(unittest.TestCase):
+    ''' some countbits tests'''
+
+    def test_simple(self):
+        ''' count bits in a simple number '''
+        self.assertEqual(2, countbits(3))
+        self.assertEqual(2, countbits2(3))
+
+    def test_zero(self):
+        ''' count bits in a simple number '''
+        self.assertEqual(0, countbits(0))
+        self.assertEqual(0, countbits2(0))
+
+    def test_all(self):
+        ''' count bits in lots of numbers '''
+        for n in range(0, 2**18):
+            self.assertEqual(countbits(n), countbits2(n))
+
+
+if __name__ == '__main__':
+    unittest.main()
