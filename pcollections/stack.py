5bdee8785fc547d2e0a596f03348471634a9647c
Pete Richardson <pete@peterichardson.com>
2012-09-03 22:05:56 -0700

    refactor tests out of stack.py into test_stack.py

--- pcollections/stack.py
+++ pcollections/stack.py
@@ -2,17 +2,19 @@
 
 '''stack.py - a simple stack implementation'''
 
-import unittest
 from pprint import pformat
 import logging
 import copy
 
+logger = logging.getLogger(__name__)
 
 class Stack:
+    '''Simple stack implementation'''
     def __init__(self):
         self.stack = []
 
     def push(self, item):
+        '''Push an item onto the stack'''
         self.stack.insert(0, item)
 
     def pop(self):
@@ -31,37 +33,5 @@ class Stack:
         return(len(self.stack))
 
     def dump(self):
-        logging.debug(pformat(self.stack))
-
-
-class TestStack(unittest.TestCase):
-    def test_push(self):
-        s = Stack()
-        self.assertEqual(s.count(), 0)
-        s.push("pete")
-        p = s.peek()
-        self.assertEqual(s.count(), 1)
-        self.assertEqual("pete", p)
-
-    def test_pop(self):
-        s = Stack()
-        s.push("Wendy")
-        s.push("Peter")
-        s.dump()
-        self.assertEqual(s.count(), 2)
-        answer = s.pop()
-        self.assertEqual(answer, "Peter")
-        self.assertEqual(s.count(), 1)
-
-    def test_count(self):
-        s = Stack()
-        self.assertEqual(0, s.count())
-        s.push("One")
-        self.assertEqual(1, s.count())
-        s.push("Two")
-        self.assertEqual(2, s.count())
-
-
-if __name__ == "__main__":
-    #logging.basicConfig(level=logging.DEBUG)
-    unittest.main()
+        '''Dump a pretty representation of the stack'''
+        logger.debug(pformat(self.stack))
---
+++ pcollections/test_stack.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+''' test_stack.py - tests for the stack class'''
+
+import unittest
+from pcollections.stack import Stack
+
+
+class TestStack(unittest.TestCase):
+    '''Testcases for Stack class'''
+    def test_push(self):
+        '''Stack - Push an item onto the stack'''
+        stack = Stack()
+        self.assertEqual(stack.count(), 0)
+        stack.push("pete")
+        topval = stack.peek()
+        self.assertEqual(stack.count(), 1)
+        self.assertEqual("pete", topval)
+
+    def test_pop(self):
+        '''Stack - Pop an item from the stack'''
+        stack = Stack()
+        stack.push("Wendy")
+        stack.push("Peter")
+        stack.dump()
+        self.assertEqual(stack.count(), 2)
+        answer = stack.pop()
+        self.assertEqual(answer, "Peter")
+        self.assertEqual(stack.count(), 1)
+
+    def test_count(self):
+        '''Stack - Count items on the stack'''
+        stack = Stack()
+        self.assertEqual(0, stack.count())
+        stack.push("One")
+        self.assertEqual(1, stack.count())
+        stack.push("Two")
+        self.assertEqual(2, stack.count())
+
+
+if __name__ == "__main__":
+    #import logging
+    #logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
