195ef257528f6caf17b8790a02b6a94013a354cf
Pete Richardson <pete@peterichardson.com>
2012-09-03 22:05:28 -0700

    improve docstrings for nosetests output in test_linkedlist.py

--- pcollections/test_linkedlist.py
+++ pcollections/test_linkedlist.py
@@ -7,13 +7,13 @@ from pcollections.linkedlist import LinkedList
 class LinkedListTest(unittest.TestCase):
     '''Test cases for linked lists'''
     def test_create(self):
-        '''Create a simple linked list'''
+        '''LinkedList - Create a simple linked list'''
         alist = LinkedList(50)
         self.assertEqual(None, alist.next)
         self.assertEqual(50, alist.value)
 
     def test_insert(self):
-        '''Insert an item into a linked list'''
+        '''LinkedList - Insert an item into a linked list'''
         alist = LinkedList(25)
         alist.insert(34)
         self.assertEqual(34, alist.value)
@@ -21,7 +21,7 @@ class LinkedListTest(unittest.TestCase):
         self.assertEqual(None, alist.next.next)
 
     def test_pop(self):
-        '''Pop an item from a linked list'''
+        '''LinkedList - Pop an item from a linked list'''
         alist = LinkedList(25)
         self.assertEqual(25, alist.value)
         alist.insert(34)
@@ -31,7 +31,7 @@ class LinkedListTest(unittest.TestCase):
         self.assertEqual(25, alist.value)
 
     def test_append(self):
-        '''Append an item to a linked list'''
+        '''LinkedList - Append an item to a linked list'''
         alist = LinkedList(17)
         self.assertEqual(17, alist.value)
         alist.append(99)
@@ -39,7 +39,7 @@ class LinkedListTest(unittest.TestCase):
         self.assertEqual(99, alist.next.value)
 
     def test_find(self):
-        '''Find an item in a linked list'''
+        '''LinkedList - Find an item in a linked list'''
         alist = LinkedList(1)
         alist.append(2)
         alist.append(3)
