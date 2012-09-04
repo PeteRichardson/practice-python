9ed063db1e89f0d108eac1a78849c7602f0c36be
Pete Richardson <pete@peterichardson.com>
2012-09-03 22:37:21 -0700

    add some docstrings

--- palgorithms/minmax.py
+++ palgorithms/minmax.py
@@ -5,80 +5,47 @@
 
 
 import logging
-import unittest
 
 
 def is_min(old_direction, new_direction):
+    '''Have we changed direction and created a local min'''
     return old_direction == -1 and new_direction == 1
 
 
 def is_max(old_direction, new_direction):
+    '''Have we changed direction and created a local max'''
     return old_direction == 1 and new_direction == -1
 
 
-def is_same(current, next):
-    return current == next
+def is_same(current, nextitem):
+    '''Are two items the same?  Just added for consistency'''
+    return current == nextitem
 
 
 def get_minmax(ints):
+    '''Take a list of ints and return local mins and maxes'''
     results = []
     current = ints[0]
     location = -1
     old_direction = 0
 
-    for next in ints[1:]:
+    for nextitem in ints[1:]:
         location = location + 1
-        logging.debug("%3d  |  %d  | %d" % (old_direction, current, next))
-        if is_same(current, next):
+        logging.debug("%3d  |  %d  | %d",
+            old_direction, current, nextitem)
+        if is_same(current, nextitem):
             # don't change old direction!
             continue
 
         # new direction = +1, 0 or -1
-        new_direction = (next - current) / abs(next - current)
+        new_direction = (nextitem - current) / abs(nextitem - current)
         if is_min(old_direction, new_direction):
             results.append((location, current, "MIN"))
         elif is_max(old_direction, new_direction):
             results.append((location, current, "MAX"))
 
         old_direction = new_direction
-        current = next
+        current = nextitem
 
     return results
 
-
-class Test_minmax(unittest.TestCase):
-
-    def test_simplemax(self):
-        ints = [4, 5, 4]
-        results = get_minmax(ints)
-        self.assertEqual(results, [(1, 5, "MAX")])
-
-    def test_simplemin(self):
-        ints = [4, 3, 4]
-        results = get_minmax(ints)
-        self.assertEqual(results, [(1, 3, "MIN")])
-
-    def test_allsame(self):
-        ints = [4, 4, 4]
-        results = get_minmax(ints)
-        self.assertEqual(results, [])
-
-    def test_allincreasing(self):
-        ints = [3, 4, 5]
-        results = get_minmax(ints)
-        self.assertEqual(results, [])
-
-    def test_alldecreasing(self):
-        ints = [5, 4, 3]
-        results = get_minmax(ints)
-        self.assertEqual(results, [])
-
-    def test_complex(self):
-        ints = [5, 7, 9, 8, 5, 4, 2, 1, 5, 6]
-        results = get_minmax(ints)
-        self.assertEqual(results, [(2, 9, "MAX"), (7, 1, "MIN")])
-
-
-if __name__ == '__main__':
-    #logging.basicConfig(level=logging.DEBUG)
-    unittest.main()
