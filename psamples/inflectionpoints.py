2530071eb6fd6a1d0de4eda71bc866af1662b279
Pete Richardson <pete@peterichardson.com>
2012-09-09 14:26:40 -0700

    Large pylint cleanup for inflectionpoints code

--- psamples/inflectionpoints.py
+++ psamples/inflectionpoints.py
@@ -1,49 +1,59 @@
 #!/usr/bin/env python
+'''inflectionpoints.py - Calculate local mins and maxes of a series of ints'''
 
-class InflectionPoints:
+
+class InflectionPointFinder:
+    '''Calculate local mins and maxes of a series of ints'''
     def __init__(self, values):
         self.values = values
 
     def __str__(self):
         return ",".join([str(v) for v in self.values])
-    
+
     def points(self):
-        for i in range(1, len(self.values)-1):
-            yield( (i, (self.values[i-1], self.values[i], self.values[i+1])) )
-            
+        '''Generate all the points in the series'''
+        for index in range(1, len(self.values) - 1):
+            yield((index,  (self.values[index - 1],
+                            self.values[index],
+                            self.values[index + 1])))
+
     @classmethod
-    def isLocalMax(cls, t):
-        return (t[0] < t[1] and t[1] > t[2])
-        
+    def is_local_max(cls, tup):
+        '''True if the center item of tuple is a local max'''
+        return (tup[0] < tup[1] and tup[1] > tup[2])
+
     @classmethod
-    def isLocalMin(cls, t):
-        return (t[0] > t[1] and t[1] < t[2])
-        
+    def is_local_min(cls, tup):
+        '''True if the center item of tuple is a local min'''
+        return (tup[0] > tup[1] and tup[1] < tup[2])
+
     @classmethod
-    def isInflectionPoint(cls, t):
-        return (cls.isLocalMax(t) or cls.isLocalMin(t))
-    
-    def inflectionPoints(self):
+    def is_inflection_point(cls, tup):
+        '''True if the center item of tuple is an inflection point'''
+        return (cls.is_local_max(tup) or cls.is_local_min(tup))
+
+    def inflection_points(self):
+        '''Return list of all inflection points
+        Maybe should be changed to a generator?'''
         results = []
-        for p in self.points():
-            i = p[0]
-            t = p[1]
-            if self.isLocalMax(t):
-                results.append((i,t[1],"Max"))
-            elif (self.isLocalMin(t)):
-                results.append((i,t[1], "Min"))
+        for pnt in self.points():
+            index = pnt[0]
+            tup = pnt[1]
+            if self.is_local_max(tup):
+                results.append((index, tup[1], "Max"))
+            elif (self.is_local_min(tup)):
+                results.append((index, tup[1], "Min"))
         return tuple(results)
 
 if __name__ == '__main__':
-    data = [(1,2,3,2,1),
-            (3,2,1,2,3,2,1),
-            (300,2,1000,5000,65,5000,1),
-            (-5,-4,-3,-4,-3,-2)]
-    
-    for points in data:
-            ips = InflectionPoints(points)
-            print "------------------"
-            print points
-            for p in ips.inflectionPoints():
-                print p
-            
+    TEST_DATA = [(1, 2, 3, 2, 1),
+            (3, 2, 1, 2, 3, 2, 1),
+            (300, 2, 1000, 5000, 65, 5000, 1),
+            (-5, -4, -3, -4, -3, -2)]
+
+    for points in TEST_DATA:
+        ipf = InflectionPointFinder(points)
+        print "------------------"
+        print points
+        for point in ipf.inflection_points():
+            print point
---
+++ psamples/test_inflection.py
@@ -0,0 +1,54 @@
+'''Test cases for inflectionpoints.py'''
+import unittest
+from inflectionpoints import InflectionPointFinder
+
+
+class InflectionTest(unittest.TestCase):
+    '''Test cases for inflectionpoints.py'''
+
+    def test_single_max(self):
+        '''test a simple single maximum in a series'''
+        ipf = InflectionPointFinder((1, 2, 3, 2, 1))
+        self.assertEqual(ipf.inflection_points(), ((2, 3, "Max"),))
+
+    def test_single_min(self):
+        '''test a simple single minimum in a series'''
+        ipf = InflectionPointFinder((3, 2, 1, 2, 3))
+        self.assertEqual(ipf.inflection_points(), ((2, 1, "Min"),))
+
+    def test_min_max(self):
+        '''test a series with both a min and a max'''
+        ipf = InflectionPointFinder((3, 2, 1, 2, 3, 2, 1))
+        self.assertEqual(ipf.inflection_points(),
+            ((2, 1, "Min"), (4, 3, "Max")))
+
+    def test_big_numbers(self):
+        '''test a few larger numbers'''
+        ipf = InflectionPointFinder((300, 2, 1000, 5000, 65, 5000, 1))
+        self.assertEqual(ipf.inflection_points(),
+            ((1, 2, "Min"), (3, 5000, "Max"), (4, 65, "Min"), (5, 5000, "Max")))
+
+    def test_all_negative_numbers(self):
+        '''test a series with all negative numbers'''
+        ipf = InflectionPointFinder((-5, -4, -3, -4, -3, -2))
+        self.assertEqual(ipf.inflection_points(),
+            ((2, -3, "Max"), (3, -4, "Min")))
+
+    def test_is_local_max(self):
+        '''some spot testing for the is_local_max function'''
+        self.assertTrue(InflectionPointFinder.is_local_max((1, 2, 1)))
+        self.assertFalse(InflectionPointFinder.is_local_max((2, 1, 2)))
+        self.assertFalse(InflectionPointFinder.is_local_max((2, 1, 1)))
+        self.assertFalse(InflectionPointFinder.is_local_max((1, 1, 2)))
+
+    def test_is_local_min(self):
+        '''some spot testing for the is_local_min function'''
+        self.assertTrue(InflectionPointFinder.is_local_min((2, 1, 2)))
+        self.assertFalse(InflectionPointFinder.is_local_min((1, 2, 1)))
+        self.assertFalse(InflectionPointFinder.is_local_min((1, 2, 2)))
+        self.assertFalse(InflectionPointFinder.is_local_min((2, 2, 1)))
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
