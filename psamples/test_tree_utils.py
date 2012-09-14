9f25d979c9d60ee13ea2c561a5d9e734f85fd9c6
Pete Richardson <pete@peterichardson.com>
2012-09-13 22:12:30 -0700

    A few different ways to implement depth() for is_balanced()

--- psamples/test_tree_utils.py
+++ psamples/test_tree_utils.py
@@ -5,15 +5,20 @@ from tree_utils import Node, is_balanced
 
 
 class Test_Node(unittest.TestCase):
+
     def test_single_node(self):
         nodeA = Node(43)
         self.assertTrue(is_balanced(nodeA))
+        self.assertEqual(nodeA.d2(), 1)
+        self.assertEqual(nodeA.d3(), 1)
 
     def test_single_child(self):
         nodeA = Node(43)
         nodeB = Node(57)
         nodeA.left = nodeB
         self.assertTrue(is_balanced(nodeA))
+        self.assertEqual(nodeA.d2(), 2)
+        self.assertEqual(nodeA.d3(), 2)
 
     def test_three_in_a_row(self):
         nodeA = Node(43)
@@ -22,6 +27,9 @@ class Test_Node(unittest.TestCase):
         nodeA.left = nodeB
         nodeB.right = nodeC
         self.assertFalse(is_balanced(nodeA))
+        self.assertFalse(nodeA.is_balanced())
+        self.assertEqual(nodeA.d2(), 3)
+        self.assertEqual(nodeA.d3(), 3)
 
 if __name__ == "__main__":
     unittest.main()
--- psamples/tree_utils.py
+++ psamples/tree_utils.py
@@ -20,3 +20,23 @@ class Node:
         self.value = value
         self.left = None
         self.right = None
+
+    def d2(self):
+        if self.left == None and self.right == None:
+            return 1
+        elif self.left == None and self.right != None:
+            return depth(self.right) + 1
+        elif self.left != None and self.right == None:
+            return depth(self.left) + 1
+        else:
+            return max(depth(self.left), depth(self.right)) + 1
+
+    def d3(self):
+        dleft = 0 if self.left == None else self.left.d3()
+        dright = 0 if self.right == None else self.right.d3()
+        return max(dleft, dright) + 1
+
+    def is_balanced(self):
+        dleft = self.left.d3() if self.left != None else 0
+        dright = self.right.d3() if self.right != None else 0
+        return abs(dleft - dright) <= 1
