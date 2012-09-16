054ebc4068663ee8c868fc3a1f9a02807c33f895
Pete Richardson <pete@peterichardson.com>
2012-09-15 22:56:59 -0700

    Working StoreCredit solution

---
+++ codejam/StoreCredit/storecredit.py
@@ -0,0 +1,36 @@
+''' storecredit.py - code for google code jam Store Credit problem '''
+
+
+class Case:
+    def __init__(self, threelines):
+        #pdb.set_trace()
+        self.C = int(threelines[0])
+        self.I = int(threelines[1])
+        self.P = [int(x) for x in (threelines[2]).split()]
+        self.solution = None
+
+    def solve(self):
+        needs = {}
+        for i in range(len(self.P)):
+            price = self.P[i]
+            #print "price =", price, 
+            if needs.has_key(price):
+                self.solution = [needs[price], i + 1]
+                #print "Found a solution!", self.solution
+                return
+            else:
+                #print "adding %d => %d" % (self.C - price, i)
+                needs[self.C - price] = i + 1
+
+
+class Store:
+    def __init__(self, inp):
+        self.lines = inp.split("\n")
+        self.N = int(self.lines[0])
+        self.cases = []
+
+        for c in range(self.N):
+            case = Case(self.lines[(c * 3) + 1:(c * 3) + 4])
+            case.solve()
+            self.cases.append(case)
+
---
+++ codejam/StoreCredit/test_storecredit.py
@@ -0,0 +1,45 @@
+''' test_storecredit.py - unit tests for google code jam problem: Store Credit '''
+
+import unittest
+from storecredit import Store
+
+
+class Test_StoreCredit(unittest.TestCase):
+
+    def test_inputsplit(self):
+        inp = '''3
+                100
+                3
+                5 75 25
+                200
+                7
+                150 24 79 50 88 345 3
+                8
+                8
+                2 1 9 4 4 56 90 3'''
+
+        store = Store(inp)
+        self.assertEqual(store.N, 3)
+        self.assertEqual(len(store.cases), 3)
+        self.assertEqual(store.cases[0].C, 100)
+        self.assertEqual(store.cases[0].I, 3)
+        self.assertEqual(len(store.cases[0].P), 3)
+        self.assertEqual(store.cases[0].P, [5, 75, 25])
+        self.assertEqual(store.cases[0].solution, [2, 3])
+
+        self.assertEqual(store.cases[1].C, 200)
+        self.assertEqual(store.cases[1].I, 7)
+        self.assertEqual(len(store.cases[1].P), 7)
+        self.assertEqual(store.cases[1].P, [150, 24, 79, 50, 88, 345, 3])
+        self.assertEqual(store.cases[1].solution, [1, 4])
+
+        self.assertEqual(store.cases[2].C, 8)
+        self.assertEqual(store.cases[2].I, 8)
+        self.assertEqual(len(store.cases[2].P), 8)
+        self.assertEqual(store.cases[2].P, [2, 1, 9, 4, 4, 56, 90, 3])
+        self.assertEqual(store.cases[2].solution, [4, 5])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
