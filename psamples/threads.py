aee7e0f350003752fd136603a170f023705896cf
Pete Richardson <pete@peterichardson.com>
2012-09-21 18:07:09 -0700

    change to logging library

--- psamples/threads.py
+++ psamples/threads.py
@@ -1,6 +1,8 @@
 ''' threads.py - simple threading practice app'''
 
 import threading
+import logging
+
 from time import sleep
 from random import random
 from Queue import Queue
@@ -16,25 +18,32 @@ class Worker(threading.Thread):
     '''Some work to do in a thread'''
     MAX_SLEEP = 2
 
-    def __init__(self, queue):
+    def __init__(self, tnum, queue):
         threading.Thread.__init__(self)
         self.queue = queue
+        self.name = "Worker {}".format(tnum)
 
     def run(self):
+        logger.info("Created {}".format(self.name))
         delay = 0
         if self.queue.qsize() == 0:
+            logger.debug("\tqueue is empty.  Going to sleep...")
             delay = my_sleep(self.MAX_SLEEP)
-        print "Hello, {}. (slept {:.2f}, depth: {})".format(self.getName(), delay, self.queue.qsize())
+        logger.debug("\twaking up after {:.2f} seconds".format(delay))
+        logger.info("Hello, {}. depth: {}".format(self.name, self.queue.qsize()))
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger()
 
     queue = Queue()
  
     for tnum in range(4):
-        t = Worker(queue)
+        t = Worker(tnum, queue)
         t.start()
 
     for item in range(5):
         delay = my_sleep(1)
-        print "Added 1"
-        queue.put("Hello")
+        msg = "Hello"
+        queue.put(msg)
+        logger.info("Added {} to queue".format(msg))
