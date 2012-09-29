81a7af45e41a9637513a920a3aa5881d9282048a
Pete Richardson <pete@peterichardson.com>
2012-09-28 18:18:30 -0700

    Added ability to greet and converse

--- psamples/agent.py
+++ psamples/agent.py
@@ -4,6 +4,19 @@
 
 import threading
 from random import random
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Message:
+    def __init__(self, agent, message):
+        self.agent = agent
+        self.message = message
+
+    def str(self):
+        '''String representation of this message'''
+        return "{}: {}".format(self.agent.name, self.message)
 
 
 class Agent(threading.Thread):
@@ -14,19 +27,39 @@ class Agent(threading.Thread):
         threading.Thread.__init__(self)
         self.name = name
         self.need_responses = []
+        self.greeted = set()
+        self.heard = 0
+        self.said = 0
 
     def say_something(self):
         '''print out a random message'''
         return self.phrases[int(random() * 4)]
 
-    def say_something_to(self, who):
-        who.hear(self.say_something())
+    def greet(self, who):
+        '''Say Hello to someone else'''
+        phrase = self.say_something()
+        LOGGER.debug("{:5} to {:5}: {}".format(self.name, who.name, phrase))
+        msg = Message(self, phrase)
+        who.hear(msg)
+        self.said += 1
+        self.greeted.add(who)
 
     def hear(self, message):
+        '''Receive a message from someone else'''
         self.need_responses.append(message)
+        self.heard += 1
+
+    def start(self):
+        while len(self.need_responses) > 0:
+            message = self.need_responses.pop()
+            if message.agent not in self.greeted:
+                self.greet(message.agent)
+
+def main():
+    '''do a basic functional test'''
+    pete = Agent("Pete")
+    pete.say_something()
 
 
 if __name__ == "__main__":
-    pete = Agent("Pete")
-    pete.say_something()
-    pete.list
+    main()
--- psamples/test_agent.py
+++ psamples/test_agent.py
@@ -3,7 +3,17 @@
 '''test_conversation.py'''
 
 import unittest
-from agent import Agent
+from agent import Agent, Message
+import logging
+
+class Test_Message(unittest.TestCase):
+
+    def test_simple(self):
+        '''Does the thing work at all'''
+        agent = Agent("Pete")
+        message = Message(agent, "Greetings")
+        self.assertEqual(message.agent.name, "Pete")
+        self.assertEqual(message.message, "Greetings")
 
 
 class Test_Agent(unittest.TestCase):
@@ -26,5 +36,15 @@ class Test_Agent(unittest.TestCase):
         amy.hear(msg)
         self.assertEqual(amy.need_responses, [msg])
 
+    def test_conversation(self):
+        '''Can two agents talk?'''
+        pete = Agent("Pete")
+        wendy = Agent("Wendy")
+        pete.greet(wendy)
+        pete.start()
+        wendy.start()
+        self.assertEqual(1, pete.heard)
+
 if __name__ == "__main__":
+    #logging.basicConfig(level=logging.DEBUG)
     unittest.main()
