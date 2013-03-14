da4f60375dd8d9a43c8520119d79d26a3868f7cb
Pete Richardson <pete@peterichardson.com>
2013-03-14 15:18:08 -0700

    added append

--- pcollections/ll2/linkedlist.py
+++ pcollections/ll2/linkedlist.py
@@ -1,28 +1,32 @@
 class LinkedList:
     def __init__(self, value=None):
         self.value = value
-        self._next = None
+        self.next = None
 
     def prepend(self, value):
         ''' create a new node and put it second.   can't put it first,
             since that's 'self'.  But it's easy enough to correctly update
             the first and second values '''
         new_node = LinkedList(self.value)
-        new_node._next = self._next
-        self._next = new_node
+        new_node._next = self.next
+        self.next = new_node
         self.value = value
 
-    def head(self):
-        return self.value
+    def append(self, value):
+        new_node = LinkedList(self.value)
+        last_node = None
+        for item in self:
+            last_node = item
+        last_node.next = new_node
 
-    def next(self):
-        return self._next
+    def peek(self):
+        return self.value
 
     def __iter__(self):
         head = self
         yield head
-        while head._next is not None:
-            head = head._next
+        while head.next is not None:
+            head = head.next
             yield head
 
     def __len__(self):
