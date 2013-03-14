778fec58877ed5c4668868c7011f2f84a69a2753
Pete Richardson <pete@peterichardson.com>
2013-03-14 15:42:52 -0700

    Handle uninitialized linked list case  (where no initial value passed to constructor)
    implies you can't store None values in objects

--- pcollections/ll2/linkedlist.py
+++ pcollections/ll2/linkedlist.py
@@ -1,3 +1,6 @@
+class ListEmptyError(ValueError):
+    pass
+
 class LinkedList:
     def __init__(self, value=None):
         self.value = value
@@ -7,20 +10,44 @@ class LinkedList:
         ''' create a new node and put it second.   can't put it first,
             since that's 'self'.  But it's easy enough to correctly update
             the first and second values '''
-        new_node = LinkedList(self.value)
-        new_node._next = self.next
-        self.next = new_node
-        self.value = value
+        if self.isEmpty():
+            self.value = value
+        else:
+            new_node = LinkedList(self.value)
+            new_node._next = self.next
+            self.next = new_node
+            self.value = value
 
     def append(self, value):
-        new_node = LinkedList(self.value)
-        last_node = None
-        for item in self:
-            last_node = item
-        last_node.next = new_node
+        if self.isEmpty():
+            self.value = value
+        else:
+            new_node = LinkedList(self.value)
+            last_node = None
+            for item in self:
+                last_node = item
+            last_node.next = new_node
 
     def peek(self):
         return self.value
+
+    def push(self, value):
+        self.prepend(value)
+
+    def pop(self):
+        if self.value is None:
+            raise ListEmptyError
+        if self.next is None:
+            return None
+        result = self.next
+        self.next = result.next
+        tmp = self.value
+        self.value = result.value
+        return tmp
+
+    def isEmpty(self):
+        return self.value is None
+        #return len(self) == 0
 
     def __iter__(self):
         head = self
@@ -30,6 +57,8 @@ class LinkedList:
             yield head
 
     def __len__(self):
+        if self.value is None:
+            return 0
         result = 0
         for item in self:
             result += 1
--- pcollections/ll2/test_linkedlist.py
+++ pcollections/ll2/test_linkedlist.py
@@ -1,25 +1,43 @@
 import unittest
-from linkedlist import LinkedList
+from linkedlist import LinkedList, ListEmptyError
 
 
 class Test_LinkedList(unittest.TestCase):
+    def setUp(self):
+        self.ll = LinkedList(4)
+        self.ll.prepend(5)
+
     def test_simple(self):
-        ll = LinkedList(4)
-        ll.prepend(5)
-        self.assertEqual(5, ll.peek())
-        self.assertEqual(2, len(ll))
+        self.assertEqual(5, self.ll.peek())
+        self.assertEqual(2, len(self.ll))
 
     def test_append(self):
-        ll = LinkedList(4)
-        ll.append(5)
-        self.assertEqual(4, ll.peek())
-        self.assertEqual(2, len(ll))
+        self.ll.append(6)
+        self.assertEqual(5, self.ll.peek())
+        self.assertEqual(3, len(self.ll))
 
     def test_len(self):
-        ll = LinkedList(4)
+        self.ll.append(6)
+        self.assertEqual(3, len(self.ll))
+
+    def test_len_uninitialized(self):
+        ll = LinkedList()
         ll.prepend(5)
-        ll.append(3)
-        self.assertEqual(3, len(ll))
+        self.assertEqual(1, len(ll))
+
+    def test_pop(self):
+        head_value = self.ll.pop()
+        self.assertEqual(5, head_value)
+        self.assertEqual(1, len(self.ll))
+        self.assertEqual(4, self.ll.peek())
+
+    def test_isEmpty(self):
+        ll = LinkedList()
+        self.assertTrue(ll.isEmpty())
+
+    def test_pop_empty(self):
+        ll = LinkedList()
+        self.assertRaises(ListEmptyError, ll.pop)
 
 if __name__ == '__main__':
     unittest.main()
