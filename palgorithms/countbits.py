eceda6d5cd250b8303a84fb57c52cf97252f32c6
Pete Richardson <pete@peterichardson.com>
2013-03-23 13:34:57 -0700

    Idiomize the count bits code.
    Changed  x == None to x is None, then to if not isinstance(x, int) which is what I really meant.

--- palgorithms/countbits.py
+++ palgorithms/countbits.py
@@ -5,25 +5,26 @@
 
 def countbits(value):
     ''' count bits by shifting one at a time '''
-    if value == None:
-        raise RuntimeError("Expected Integer, Got None")
+    if not isinstance(value, int):
+        raise TypeError("countbits requires an integer")
     bitcount = 0
-    while value > 0:
-        bitcount = bitcount + (value & 0x1)
-        value = value >> 1
+    while value:
+        bitcount += (value & 0x1)
+        value >>= 1
     return bitcount
-
-lookup_table = [0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4]
 
 
 def countbits2(value):
     ''' count bits by shifting 4 at a time '''
-    if value == None:
-        raise RuntimeError("Expected Integer, Got None")
+
+    lookup_table = [0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4]
+
+    if not isinstance(value, int):
+        raise TypeError("countbits2 requires an integer")
     bitcount = 0
-    while value > 0:
-        bitcount = bitcount + lookup_table[value & 0xF]
-        value = value >> 4
+    while value:
+        bitcount += lookup_table[value & 0xF]
+        value >>= 4
     return bitcount
 
 if __name__ == '__main__':
