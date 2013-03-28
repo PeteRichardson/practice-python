21982c0b5b464a2e0f2b09483f255ce21458940a
Pete Richardson <pete@peterichardson.com>
2013-03-28 16:42:56 -0700

    flowers.py - simple script to dump /usr/share/misc/flowers

---
+++ psamples/flowers.py
@@ -0,0 +1,14 @@
+import string
+
+for line in open("/usr/share/misc/flowers", "r").readlines():
+    if line[0] == '#':
+        continue
+    (flower, meaning) = line.strip().split(":")
+    if "," in flower:
+        (fname, fadjective) = flower.split(",", 1)
+        fadjective = string.capwords(fadjective.strip().replace(",",""))
+        if fadjective:
+           fname = " ".join([fadjective, fname])
+        flower = fname
+    print "'{0}' means '{1}'".format(flower, meaning)
+
