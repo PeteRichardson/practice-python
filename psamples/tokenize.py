6e811c9822d0a3a50d5d2dcfc655c7b270e1f6c1
Pete Richardson <pete@peterichardson.com>
2013-03-27 17:10:45 -0700

    Added memory mapped files so we don't read everything into memory.
    (Actually, I'm not sure if for line in open().readlines() uses iterators (and therefore doesn't read everything into memory…)

--- psamples/tokenize.py
+++ psamples/tokenize.py
@@ -1,18 +1,44 @@
-import glob
 from pprint import pprint
 import nltk.tokenize.regexp
+import os
+import contextlib
+import mmap
+import sys
+
+def get_file_list(top):
+    for (root, _, files) in os.walk(top):
+        for filename in files:
+            (basename, ext) = os.path.splitext(filename)
+            if ext.lower() == ".py":
+                yield os.path.join(root, filename)
+
+
+def parse_one_file_small(filename, tokenizer):
+    words = set()
+    if os.stat(filename).st_size:
+        f = open(filename, "r+")
+        with contextlib.closing(mmap.mmap(f.fileno(), 0)) as mapped_file:
+            words.update(tokenizer.tokenize(mapped_file))
+    return words
 
 
 def parse_one_file(filename, tokenizer):
     words = set()
-    for line in open(filename, "r").readlines():
-        words.update(tokenizer.tokenize(line))
-        #words.update(nltk.word_tokenize(line))
+    if os.stat(filename).st_size:
+        for line in open(filename, "r").readlines():
+            words.update(tokenizer.tokenize(line))
     return words
 
-allwords = set()
-tokenizer = nltk.RegexpTokenizer('[.!\(\)\[\]*&^%=|\,\{\}\s\'\:\"]+', gaps=True)
-for filename in glob.glob("*.py"):
-    filewords = parse_one_file(filename, tokenizer)
-    allwords |= set(filewords)
-pprint(allwords)
+def main(top):
+    assert os.path.exists(top), "# FATAL ERROR: No such path as {top}"
+    allwords = set()
+    tokenizer = nltk.tokenize.regexp.RegexpTokenizer('[.!\(\)\[\]*&^%=|\,\{\}\s\'\:\"]+', gaps=True)
+    for filename in get_file_list(top):
+        filewords = parse_one_file_small(filename, tokenizer)
+        allwords |= set(filewords)
+    pprint(allwords)
+    return 0
+
+if __name__ == '__main__':
+    top = "/Users/pete/projects/practice/"
+    sys.exit(main(top))
