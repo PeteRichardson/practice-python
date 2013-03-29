70011f9fd2257c289422f93e69d1cb67dfbd0f00
Pete Richardson <pete@peterichardson.com>
2013-03-28 17:06:01 -0700

    retest.py - added option parsing with optparse.
    supports -p pattern and -i option to ignore case

--- psamples/retest.py
+++ psamples/retest.py
@@ -1,8 +1,16 @@
 import re
 from fileinput import FileInput
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option('-p', dest="pattern", action="store")
+parser.add_option('-i', action="store_true")
+options = parser.parse_args()[0]
+
+flags = re.IGNORECASE if options.i else 0
 
 files = ["/usr/share/misc/flowers"]
-p = re.compile("love")
+p = re.compile(options.pattern, flags)
 
 for line in FileInput(files):
     if p.search(line):
