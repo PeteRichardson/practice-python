56a7042be06030ac68c5efeb42abbc8c5c1792e9
Pete Richardson <pete@peterichardson.com>
2013-05-11 15:52:35 -0700

    udpsend - sample of sending udp packets in python

---
+++ psamples/udpsend.py
@@ -0,0 +1,12 @@
+import socket
+
+UDP_IP = "192.168.1.220"
+UDP_PORT = 6718
+MESSAGE = "Hello, World!"
+
+print "UDP target IP:", UDP_IP
+print "UDP target port:", UDP_PORT
+print "message:", MESSAGE
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
